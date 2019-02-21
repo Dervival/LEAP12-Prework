43f0119a9c0d90828e99ba0e7676a37ce7d1982b
Blaise Clarke <blaiserclarke@gmail.com>
2019-02-21 12:54:28 -0800

    Finished prework through Loops/foreach and slides 27/57

--- SoftwareEngFundPartTwo/SoftwareEngFundPartTwo/Program.cs
+++ SoftwareEngFundPartTwo/SoftwareEngFundPartTwo/Program.cs
@@ -12,9 +12,14 @@ namespace SoftwareEngFundPartTwo
             //OrderOfOperations("hey");
             //OrderOfOperations("hello there person");
             //OrderOfOperations("|||||||||||||||");
-            OrderOfOperationsNested("hey");
-            OrderOfOperationsNested("hello there person");
-            OrderOfOperationsNested("|||||||||||||||");
+            //OrderOfOperationsNested("hey");
+            //OrderOfOperationsNested("hello there person");
+            //OrderOfOperationsNested("|||||||||||||||");
+            //InfiniteWhileLoop();
+            PullOddsFromStringWhile();
+            PullOddsFromStringDoWhile();
+            PullOddsFromStringFor();
+            PullOddsFromStringForeach();
         }
 
         /// <summary>
@@ -113,6 +118,98 @@ namespace SoftwareEngFundPartTwo
             {
                 Console.WriteLine("The string is neither 1 character, 3 characters, nor an even number greater than 8 characters long - " + input.Length);
             }
+        }
+
+        /// <summary>
+        /// Function for showing an infinite while loop.
+        /// </summary>
+        static void InfiniteWhileLoop()
+        {
+            while (true)
+            {
+                //creates an infinite loop
+            }
+        }
+
+        /// <summary>
+        /// Function for pulling digits pertaining to odd numbers out of a string, using a while loop. There's probably a better way than using bracket notation and forcing the character into a string, but this seems to work.
+        /// </summary>
+        static void PullOddsFromStringWhile()
+        {
+            string s = "0123456789";
+            int index = 0;
+            while(index < s.Length)
+            {
+                //This is a bit ugly, but TryParse didn't seem to have an overload for a character in a string using bracket notation, and chars apparently cannot be explicitly cast to strings (but chars can be appended to empty strings!)
+                string digit = "";
+                digit += s[index];
+                Int32.TryParse(digit, out int d);
+                if(d % 2 == 1)
+                {
+                    Console.Write(d);
+                }
+                index++;
+            }
+            Console.WriteLine("\n");
+        }
+
+        /// <summary>
+        /// Function for pulling odd number digits out of a string using a do...while loop.
+        /// </summary>
+        static void PullOddsFromStringDoWhile()
+        {
+            string s = "0123456789";
+            int index = 0;
+            do
+            {
+                string digit = "";
+                digit += s[index];
+                Int32.TryParse(digit, out int d);
+                if (d % 2 == 1)
+                {
+                    Console.Write(d);
+                }
+                index++;
+            }
+            while (index < s.Length);
+            Console.WriteLine("\n");
+        }
+
+        /// <summary>
+        /// Function for pulling odd number digits out of a string using a for loop.
+        /// </summary>
+        static void PullOddsFromStringFor()
+        {
+            string s = "0123456789";
+            for(int i = 0; i < s.Length; i++)
+            {
+                string digit = "";
+                digit += s[i];
+                Int32.TryParse(digit, out int d);
+                if (d % 2 == 1)
+                {
+                    Console.Write(d);
+                }
+            }
+            Console.WriteLine("\n");
+        }
+
+        /// <summary>
+        /// Function for pulling odd number digits out of a string using a foreach loop. Strings are a collection of characters!
+        /// </summary>
+        static void PullOddsFromStringForeach()
+        {
+            string s = "0123456789";
+            foreach(char c in s)
+            {
+                string digit = "" + c;
+                Int32.TryParse(digit, out int d);
+                if(d % 2 == 1)
+                {
+                    Console.Write(d);
+                }
+            }
+            Console.WriteLine("\n");
         }
     }
     enum Placement

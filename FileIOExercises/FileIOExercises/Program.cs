7c29f8ec33c44859273d3c01f9661014567a7397
Blaise Clarke <blaiserclarke@gmail.com>
2019-02-21 17:05:21 -0800

    Instatiated files, completed reading text files with file class and streamreader class

---
+++ FileIOExercises/FileIOExercises/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadViaFileClass();
+            ReadViaStreamReader();
+        }
+
+        static void ReadViaFileClass()
+        {
+            //string path = @"C:\Users\oobitydoo\LEAP\LEAP12 - Prework\fileioread.txt"; //original path - I think it disliked spaces in the path
+            string path = @"C:\Users\oobitydoo\Documents\fileioread.txt";
+            string pathContents = File.ReadAllText(path);
+            Console.WriteLine(pathContents);
+        }
+
+        static void ReadViaStreamReader()
+        {
+            string path = @"C:\Users\oobitydoo\Documents\fileioread.txt";
+            StreamReader streamReader = new StreamReader(path);
+            while (!streamReader.EndOfStream)
+            {
+                var readLine = streamReader.ReadLine();
+                Console.WriteLine(readLine);
+            }
+            streamReader.Close();
+        }
+    }
+}

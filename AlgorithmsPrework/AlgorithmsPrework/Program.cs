4d89d8ac1bf5eb0681a2cfe9a213cb13b43069b6
Blaise Clarke <blaiserclarke@gmail.com>
2019-02-22 14:11:35 -0800

    Added brute force approach for IsFirstCharRepeated

--- AlgorithmsPrework/AlgorithmsPrework/Program.cs
+++ AlgorithmsPrework/AlgorithmsPrework/Program.cs
@@ -2,11 +2,33 @@
 
 namespace AlgorithmsPrework
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Checks the given string to see if the first character is repeated elsewhere in the string. Brute-force approach.
+        /// </summary>
+        /// <param name="toCheck">String to check if the first character is repeated elsewhere.</param>
+        /// <returns>True if the first character is found elsewhere in the string, false otherwise.</returns>
+        public static bool IsFirstCharRepeated(string toCheck)
+        {
+            if(toCheck.Length < 1)
+            {
+                return false;
+            }
+            char firstChar = toCheck[0];
+            for(int i = 1; i < toCheck.Length; i++)
+            {
+                if(toCheck[i] == firstChar)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
--- AlgorithmsPrework/AlgorithmsUnitTesting/UnitTest.cs
+++ AlgorithmsPrework/AlgorithmsUnitTesting/UnitTest.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using AlgorithmsPrework;
 
 namespace AlgorithmsUnitTesting
 {
     public class UnitTest
     {
         [Fact]
-        public void Test1()
+        public void IsFirstCharRepeatedReturnsTrueIfFirstCharIsInStringTwice()
         {
+            Assert.True(Program.IsFirstCharRepeated("haha"));
+        }
 
+        [Fact]
+        public void IsFirstCharRepeatedReturnsFalseIfFirstCharIsUnique()
+        {
+            Assert.False(Program.IsFirstCharRepeated("ABCDEFGH"));
+        }
+
+        [Fact]
+        public void IsFirstCharRepeatedReturnsFalseOnEmptyString()
+        {
+            Assert.False(Program.IsFirstCharRepeated(""));
         }
     }
 }
